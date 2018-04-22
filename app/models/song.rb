4075c926620e836cb083b2673a81507cac0d78ed
Sharmina Akhtar <sakht318@gmail.com>
2018-04-22 10:01:57 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -21,7 +21,7 @@ class Song < ActiveRecord::Base
 
   def note_contents=(notes)
     notes.each do |note|
-       song = Song.find_or_create_by(note)
+       song = Song.find_or_create_by(id: id)
        self.notes << note if note.strip != ''
      end
   end
